b615be53015e86dba0d07034efd303fa6a11b121
Alessandro Colleoni <a.colleoni@contrader.it>
2023-04-03 11:48:18 +0200

    Espongo endpoint per uploads

--- pyjm/settings.py
+++ pyjm/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (here: uploads)
+MEDIA_ROOT= os.path.join(BASE_DIR, "uploads")
+MEDIA_URL="/uploads/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
